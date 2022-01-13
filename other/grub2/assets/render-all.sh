89cd5e5d98c321ae3925d31ead2155f8b16dd0ff
Mohammad Javad Naderi <mjnaderi@gmail.com>
2022-01-13 23:20:08 +0330

    Fix a bug in copying some distro logos

--- other/grub2/assets/render-all.sh
+++ other/grub2/assets/render-all.sh
@@ -5,7 +5,7 @@ RESOLUTIONS=("1080p" "2k" "4k")
 
 for theme in "${THEMES[@]}"; do
   for resolution in "${RESOLUTIONS[@]}"; do
-    echo "./render-assets.sh \"$theme\" \"$resolution\": "
+    echo "./render-core.sh \"$theme\" \"$resolution\": "
     ./render-core.sh "$theme" "$resolution"
   done
 done
