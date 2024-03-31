a5c8b8c152ec2722fe008a56f24aeb84c68f54c4
Cornelius Andreas Rosenaa <cornelius@Cofftop.home>
2024-04-01 00:12:50 +0200

    Changed wallpaper script to transfer gnome dark light XML files for dark/light switching

--- wallpaper/install-wallpapers.sh
+++ wallpaper/install-wallpapers.sh
@@ -2,6 +2,7 @@
 
 REPO_DIR="$(cd "$(dirname "$0")" && pwd)"
 WALLPAPER_DIR="$HOME/.local/share/backgrounds"
+XML_DIR="$HOME/.local/share/gnome-background-properties"
 
 THEME_NAME='Graphite'
 THEME_VARIANTS=('' '-nord')
@@ -35,6 +36,10 @@ prompt () {
   esac
 }
 
+old_string="@BACKGROUNDDIR@"
+
+new_filepath="$HOME/.local/share/backgrounds/"
+
 install() {
   local theme="$1"
 
@@ -42,6 +47,10 @@ install() {
   mkdir -p "${WALLPAPER_DIR}"
 
   cp -rf ${REPO_DIR}/${THEME_NAME}${theme}/*.png ${WALLPAPER_DIR}
+  cp -rf ${REPO_DIR}/${THEME_NAME}${theme}/*.xml ${XML_DIR}
+  for file in "$XML_DIR"/*; do
+    sed -i "s/$old_string/$(printf '%s\n' "$new_filepath" | sed 's/[\/&]/\\&/g')/g" "$file"
+  done
 }
 
 if [[ "${#themes[@]}" -eq 0 ]] ; then
